44f0c59d83bf0fae8951c0fd859a9eb2232fc9f7
A-Shariatix <1384asha1384@gmail.com>
2025-07-04 16:47:44 +0330

    Update Util.py

--- Util.py
+++ Util.py
@@ -1,8 +1,18 @@
-def check_user_existence(username, password, cursor):
-user_exist = cursor.execute(f'select username, password '
-f'from information where username = "{username}"'
-f' and password = "{password}"')
-if user_exist == 2:
-return True
-else:
-return False
+import yaml
+  class Config:
+def __init__(self, data):
+self.data = data
+self.host = self.load_host()
+self.port = self.load_port()
+self.encoding = self.load_encoding()
+  def load_host(self):
+return self.data["info"]["host"]
+  def load_port(self):
+return self.data["info"]["port"]
+  def load_encoding(self):
+return self.data["encoding"]
+  def read_config_data():
+with open('yamlpy.yml', 'r') as stream:
+data = yaml.safe_load_all(stream)
+data_listed = list(data)[0]
+return data_listed
